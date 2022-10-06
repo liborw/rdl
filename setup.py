ec9a411c401a7e62a9b52c6e4659877964d3f572
Libor Wagner (ohj8Ze) <libor.wagner@cvut.cz>
2022-10-06 17:22:56 +0200

    Fix name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='rddsl',
-    version='0.1.0',
-    packages=find_packages(include=['rddsl', 'rddsl.*']),
+    name='rdl',
+    version='0.2',
+    packages=find_packages(include=['rdl', 'rdl.*']),
 )
